576a0ef220217ffa672e644a1ab77fd93746e07b
lickos <george.chios@gmail.com>
2017-07-21 01:56:14 +0300

    1234

--- src/components/weather/weather.ts
+++ src/components/weather/weather.ts
@@ -1,7 +1,7 @@
-import { Component } from "@angular/core";
-import { Http } from "@angular/http";
-import "rxjs/add/operator/map";
-import { Geolocation } from "@ionic-native/geolocation";
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Geolocation } from '@ionic-native/geolocation';
 
 /**
  * Generated class for the WeatherComponent component.
@@ -10,11 +10,12 @@ import { Geolocation } from "@ionic-native/geolocation";
  * for more info on Angular Components.
  */
 @Component({
-  selector: "weather",
-  templateUrl: "weather.html"
+  selector: 'weather',
+  templateUrl: 'weather.html'
 })
 export class WeatherComponent {
-  text: string;
+  text: any;
+  source: any;
   latitude: any;
   longitude: any;
 
@@ -25,18 +26,17 @@ export class WeatherComponent {
         this.latitude = resp.coords.latitude;
         this.longitude = resp.coords.longitude;
       })
-      .catch(error => {
-        console.log("Error getting location", error);
-      })
       .then(() => {
         let url =
-          "http://api.apixu.com/v1/current.json?key=c1a5dde8e7e54b13ad7175606172007&q=" +
+          'http://api.apixu.com/v1/current.json?key=c1a5dde8e7e54b13ad7175606172007&q=' +
           this.latitude +
-          "," +
+          ',' +
           this.longitude;
         this.http.get(url).map(res => res.json()).subscribe(data => {
-          this.text = data;
           console.log(data);
+          this.text = data.location.name;
+          this.source = data.current.condition.icon;
+          console.log(this.source);
         });
       });
   }
