21b0cfdf5e36671c15c0d6042f3a45ce5cbec45c
lickos <george.chios@gmail.com>
2017-07-25 00:43:19 +0300

    mainP

--- src/components/list-item/list-item.ts
+++ src/components/list-item/list-item.ts
@@ -8,14 +8,23 @@ export class ListItemComponent {
   @Input('imgSrc') imgSrc;
   @Input('typehead') typeHeader;
   @Input('typeText') typeText;
+  @Input('typeSubText') typeSubText;
+  @Input('typeCat') typeCat;
+  @Input('typeColorBadge') typeColorBadge;
 
   srcImg: string;
   classHead: string;
   classText: string;
+  classSubText: string;
+  classCat: string;
+  classColorBadge: string;
 
   constructor() {
     this.srcImg = this.imgSrc;
     this.classHead = this.typeHeader;
     this.classText = this.typeText;
+    this.classSubText = this.typeSubText;
+    this.classCat = this.typeCat;
+    this.classColorBadge = this.typeColorBadge;
   }
 }
