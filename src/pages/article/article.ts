ed844e681ea205fa2f7e1a2538b23538c7443f28
lickos <george.chios@gmail.com>
2017-07-28 01:28:19 +0300

    ArtCypOk

--- src/pages/article/article.ts
+++ src/pages/article/article.ts
@@ -16,9 +16,38 @@ export class ArticlePage {
 
   ionViewDidLoad() {
     console.log(this.items);
-    let artCont = this.items.content;
     this.el = document.getElementById('test');
-    this.el.innerHTML = artCont;
+    let newArt = '';
+    let finalArt = '';
+    let artCont = this.items.content;
+    let stringToCheck1 = 'width="854"';
+    let stringToCheck2 = 'height="480"';
+    let stringToCheck3 = 'src="/sites'
+    let var3 = new RegExp(stringToCheck3, 'g');
+    if(artCont.includes(stringToCheck1)){
+      console.log("1");
+      newArt = artCont.replace(stringToCheck1,'width="100%"');
+      finalArt = newArt.replace(stringToCheck2,'height="auto"');
+      if(finalArt.includes(stringToCheck3)) {
+        console.log("2")
+        let finalArtWithPic = finalArt.replace(stringToCheck3, 'src="https://alphanews.live/sites');
+        this.el.innerHTML = finalArtWithPic
+      } else {
+        console.log("3")
+        this.el.innerHTML = finalArt;
+      }
+    } else if(artCont.includes(stringToCheck3)) {
+      console.log("3");
+      let artContWithPic = artCont.replace(var3, 'src="https://alphanews.live/sites');
+      console.log(artContWithPic)
+      this.el.innerHTML = artContWithPic;
+    } else {
+      this.el.innerHTML = artCont;
+    }
+    
+    console.log(artCont);
+
+    
   }
 
 }
